1210a09f1ae1bf00708bf28f2fdbfd1be0b83b16
kaybet35 <166719352+kaybet35@users.noreply.github.com>
2025-11-13 11:15:09 +0300

    8432743237130617431

--- Etc/UHTHeaderDump/EngineSettings/Private/GeneralProjectSettings.cpp
+++ Etc/UHTHeaderDump/EngineSettings/Private/GeneralProjectSettings.cpp
@@ -5,7 +5,7 @@ UGeneralProjectSettings::UGeneralProjectSettings() {
     this->CompanyDistinguishedName = TEXT("CN=Clapfoot, O=Clapfoot, L=Toronto, S=Ontario, C=CA");
     this->CopyrightNotice = TEXT("Copyright Clapfoot Inc");
     this->ProjectName = TEXT("Foxhole Prototype");
-    this->ProjectVersion = TEXT("1.62.11.4");
+    this->ProjectVersion = TEXT("1.62.11.5");
     this->ProjectDisplayedTitle = FText::FromString(TEXT("{GameName}"));
     this->bShouldWindowPreserveAspectRatio = true;
     this->bUseBorderlessWindow = false;
